784fee41de8b229d0cc62e7b6dd6abfa16b23e67
EduardoDamian9302 <rostro-rc@hotmail.com>
2025-07-25 01:05:21 -0600

    Inicio de componente head

--- src/Components/Head.tsx
+++ src/Components/Head.tsx
@@ -1,29 +1,75 @@
-import React from "react";
-import { Offcanvas, Button } from "react-bootstrap";
-import { useState } from "react";
-const Head: React.FC = () => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  return (
-    <div className="head-container">
-      <header className="head-header">
-        <h1>Welcome to the Head Component</h1>
-        <Button variant="primary" onClick={handleShow}>
-          Open Menu
-        </Button>
-      </header>
+import { Button, Navbar, Container, NavDropdown } from "react-bootstrap";
+import styles from "./Head.module.css";
+import React, { useState } from "react";
+import OffCanvas from "./OffCanvas.tsx";
 
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Menu</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          {/* Add your menu items here */}
-          <p>Menu Item 1</p>
-          <p>Menu Item 2</p>
-        </Offcanvas.Body>
-      </Offcanvas>
+const Head: React.FC = () => {
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+
+  const handleOpen = () => setShowOffCanvas(true);
+  const handleClose = () => setShowOffCanvas(false);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          background: "#0c231e",
+          color: "#ffffff",
+          textAlign: "center",
+          fontWeight: "bold",
+          padding: "4px 0",
+          letterSpacing: "1px",
+          fontSize: "1.1rem",
+        }}
+      >
+        <img
+          src="/logo192.png"
+          width="32"
+          height="32"
+          className={styles.logo}
+          alt="React logo"
+        />
+        <div className={styles.navDropdown}>
+          <NavDropdown title="Tramites" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Tramite 1</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Tramite 2</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Tramite 3</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Informacion" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">
+              Informacion 1
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Informacion 2
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">
+              Informacion 3
+            </NavDropdown.Item>
+          </NavDropdown>
+        </div>
+      </div>
+      <Navbar expand="lg" className={styles.navbar}>
+        <Container className={styles.navbarContainer}>
+          <Navbar.Brand href="#home" className={styles.navbarBrand}>
+            React-CID
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Button variant="outline-light" onClick={handleOpen}>
+              Abrir dropdown
+            </Button>
+            <OffCanvas show={showOffCanvas} handleClose={handleClose} />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
---
+++ src/Components/OffCanvas.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import styles from "./OffCanvas.module.css";
+
+interface OffCanvasProps {
+  show: boolean;
+  handleClose: () => void;
+}
+
+const OffCanvas: React.FC<OffCanvasProps> = ({ show, handleClose }) => {
+  return (
+    <Offcanvas show={show} onHide={handleClose} className={styles.offcanvas}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Menu</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <p>Contenido del menú desplegable</p>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
+};
+
+export default OffCanvas;
